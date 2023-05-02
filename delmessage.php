e921f6a7ca4d8f6a9d50ea6f8a33c7b4d24f517f
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-05-03 00:58:59 +0800

    新增刪除全部留言

---
+++ delmessage.php
@@ -0,0 +1,32 @@
+<?php
+include("configure.php");  //抓取conifgure.php 的資料庫
+$link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password); //用$link連結 連接資料庫
+
+
+$delmess = "";
+$delmessH = "";
+
+if (isset($_POST["delmess"])) {
+    if (isset($_POST["delmessH"])) {
+        $delmess = $_POST["delmess"];
+        $delmessH = $_POST["delmessH"];
+        
+        $sql = "DELETE FROM `bookstore`.`message` Where  `PicNum` = $delmessH";
+        $stmt = $link->prepare($sql);
+        $stmt->execute();
+    }
+}
+// $stmt->bindValue(':sn', $delmess);
+// $stmt->bindValue(':sn1', $delmessH);
+
+
+// if (isset($_POST["deleteOne"])) {
+//     if (isset($_POST["DeleteP"])) {
+//       $DeleteP = $_POST["DeleteP"];
+  
+//       $pic_db_one = "DELETE FROM `bookstore`.`images` WHERE `PicNum` = $DeleteP";
+//       $sth = $link->prepare($pic_db_one);
+//       $sth->execute();
+//     }
+//   }
+  
