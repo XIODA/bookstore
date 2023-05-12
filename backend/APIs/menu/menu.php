1e6b6234f00c53c56185e68992814e5a2b2028d3
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-05-12 13:20:31 +0800

    修改分類依據使用者增加分類

--- backend/APIs/menu/menu.php
+++ backend/APIs/menu/menu.php
@@ -1,15 +1,21 @@
 <?php
 include("../../../configure.php");  //抓取conifgure.php 的資料庫
 $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password); //用$link連結 連接資料庫
+session_start();
+$ID = "";
 
+if (isset($_SESSION['ID'])) { //如果儲存到ID 便取得存取ID的變數
+    $ID = $_SESSION['ID'];
+}
 
 if(isset($_POST['dropdown'])){
 $dropdown = $_POST['dropdown'];
 }
 
 
-$query = 'SELECT * FROM `bookstore`.`menu` ';
+$query = 'SELECT * FROM `bookstore`.`menu` where `UserID` = :userId';
 $stmt = $link->prepare($query);
+$stmt->bindValue(':userId',$ID);
 $stmt->execute();
 
 
