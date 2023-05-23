99dded08211e8b95a421631fc00b0723ca637dd1
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-05-23 22:05:32 +0800

    新增全部類別

--- backend/APIs/menu/menushow.php
+++ backend/APIs/menu/menushow.php
@@ -4,12 +4,12 @@ $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=u
 session_start();
 
 if (isset($_POST['showPicH'])) {
-    $showPicH = $_POST['showPicH'];
+  $showPicH = $_POST['showPicH'];
 }
 
 $ID = "";
 if (isset($_SESSION['ID'])) { //如果儲存到ID 便取得存取ID的變數
-    $ID = $_SESSION['ID'];
+  $ID = $_SESSION['ID'];
 }
 
 $startNumber = 1;
@@ -20,12 +20,48 @@ if (isset($_GET['page'])) {
 // echo $ID;
 // exit;
 
-$query = 'SELECT * FROM `bookstore`.`images` where `UserID` = :s_userId and `Idmenu` = :s_menuId  limit :sp,6';
-$stmt = $link->prepare($query);
-$stmt->bindValue(':sp', $startNumber, PDO::PARAM_INT); //PDO::PARAM_INT = 數字格式
-$stmt->bindValue(':s_menuId',$showPicH);
-$stmt->bindValue(':s_userId',$ID);
-$stmt->execute();
+if($showPicH != 0){
+  $query = 'SELECT * FROM `bookstore`.`images` where `UserID` = :s_userId and `Idmenu` = :s_menuId  limit :sp,6';
+  // $query = 'SELECT * FROM `bookstore`.`images` ';
+  $stmt = $link->prepare($query);
+  $stmt->bindValue(':sp', $startNumber, PDO::PARAM_INT); //PDO::PARAM_INT = 數字格式
+  $stmt->bindValue(':s_menuId', $showPicH);
+  $stmt->bindValue(':s_userId', $ID);
+  $stmt->execute();
+  
+  $reasult = $stmt->fetchall(PDO::FETCH_ASSOC);
+  echo json_encode($reasult);
 
-$reasult = $stmt->fetchall(PDO::FETCH_ASSOC);
-echo json_encode($reasult);
+}else{
+  $query = 'SELECT * FROM `bookstore`.`images` where `UserID` = :s_userId  limit :sp,6';
+  // $query = 'SELECT * FROM `bookstore`.`images` ';
+  $stmt = $link->prepare($query);
+  $stmt->bindValue(':sp', $startNumber, PDO::PARAM_INT); //PDO::PARAM_INT = 數字格式
+  // $stmt->bindValue(':s_menuId', $showPicH);
+  $stmt->bindValue(':s_userId', $ID);
+  $stmt->execute();
+  
+  $reasult = $stmt->fetchall(PDO::FETCH_ASSOC);
+  echo json_encode($reasult);
+}
+
+
+if (false) {
+  $query = 'SELECT * FROM `bookstore`.`images` where `UserID` = :s_userId';
+  if ($showPicH != 0) {
+    // $query = $query . " and `Idmenu` = :s_menuId  limit :sp,6";
+    $query .= " and `Idmenu` = :s_menuId";
+  }
+  $query .= " limit :sp,6";
+  // $query = 'SELECT * FROM `bookstore`.`images` ';
+  $stmt = $link->prepare($query);
+  $stmt->bindValue(':sp', $startNumber, PDO::PARAM_INT); //PDO::PARAM_INT = 數字格式
+  if ($showPicH != 0) {
+    $stmt->bindValue(':s_menuId', $showPicH);
+  }
+  $stmt->bindValue(':s_userId', $ID);
+  $stmt->execute();
+  
+  $reasult = $stmt->fetchall(PDO::FETCH_ASSOC);
+  echo json_encode($reasult);
+}
