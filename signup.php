2402c7c9682ffd4273208ec197ffdaa72c0c5dac
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-05-29 20:25:20 +0800

    註冊頁面更新

--- signup.php
+++ signup.php
@@ -3,29 +3,62 @@ include("configure.php");
 $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
 ?>
 
-<!DOCTYPE html>
-<html lang="zh-TW">
+<!doctype html>
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>書庫</title>
+	<title>Sign up</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
 </head>
 
-<body>
-    <form action="#" method="post">
-        <div>請輸入帳號:</div>
-        <input type="text" name="account" placeholder="請輸入帳號" minlength="8">
-        <div>請輸入密碼:</div>
-        <input type="password" name="password" minlength="8">
-        <div>請輸入姓名:</div>
-        <input type="text" name="name">
+<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">註冊</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap p-0">
+						
+						<form action="" method="POST">
+							<form action="#" class="signin-form">
+								<div class="form-group">
+									<input type="text" name="account" class="form-control" placeholder="Account" required>
+								</div>
+								<div class="form-group">
+									<input id="password-field" type="password" name="password" class="form-control" placeholder="Password" required>
+									<span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+								</div>
+								<div class="form-group">
+								<input type="text" name="name" class="form-control" placeholder="Username" required>
+								</div>
+								<div class="form-group">
+									<button type="submit" class="form-control btn btn-primary submit px-3" value="Sign up">Sign up</button>
+								</div>
+							</form>
+						</form>
+						
+						</form>
+						
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-        <br />
-        <input type="submit" value="註冊">
-    </form>
-    <?php
+	<?php
     $at = "";
     $pd = "";
     $name = "";
@@ -65,6 +98,7 @@ $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=u
             $sth->execute($data);
             exec($account_db);
             echo "<script>alert('註冊成功')</script>";
+			header("Location: ./login.php");
         } else {
             echo "<script>alert('此帳號已被註冊過')</script>";
         }
@@ -74,10 +108,22 @@ $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=u
 
 
     } else if ($at == null || $pd == null || $name == null) {
-        echo "<script>alert('請輸入完整資料內容')</script>";
+        // echo "<script>alert('請輸入完整資料內容')</script>";
     }
 
     ?>
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+
+	
+
+
+
+
+
 </body>
 
 </html>
